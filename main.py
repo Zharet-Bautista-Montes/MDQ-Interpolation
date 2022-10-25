7c4bfcef7c3e79664ac07665f90f563afcdd3041
Zharet Bautista Montes <zd.bautista@uniandes.edu.co>
2022-10-25 13:18:09 -0500

    Prueba con versión analítica, resultados no deseados

--- main.py
+++ main.py
@@ -46,12 +46,12 @@ def computeDegree(X, Y):
         listflag.append(flag)
         listm.append(prev_m)
     if straightdots > 0 and flag != 0: degree += straightdots
-    print('Verificacion de grados:', listdegree)
-    print('Verificacion de banderas:', listm)
-    print('Verificacion de banderas:', listflag)
+    #print('Verificacion de grados:', listdegree)
+    #print('Verificacion de banderas:', listm)
+    #print('Verificacion de banderas:', listflag)
     return degree
 
-def buildNsolveSystem(d, X, Y):
+def buildSystem(X, Y, d):
     Xmatrix, Yvector = [], []
     for i in range(len(Y)):
         Xline = []
@@ -63,9 +63,15 @@ def buildNsolveSystem(d, X, Y):
     print('Matrix A: \n', Xmatrix)
     Yvector = np.array(Yvector)
     print('Vector b:', Yvector)
-    quoef = np.linalg.lstsq(Xmatrix, Yvector, rcond=None)
-    print('Quoefficients:', quoef[0])
-    return quoef[0]
+    return Xmatrix, Yvector
+
+def printCheck(XM, XMT, XM2, INV, XMY, TOTAL):
+    print('Original matrix: \n', XM)
+    print('Transpose: \n', XMT)
+    print('Product: \n', XM2)
+    print('Inverse of product: \n', INV)
+    print('Transpose times Y: \n', XMY)
+    print('Total: \n', TOTAL)
 
 if __name__ == '__main__':
     option, Xcoords, Ycoords = '', [], []
@@ -73,7 +79,7 @@ if __name__ == '__main__':
         option = input('¿Desea leer las coordenadas de un archivo (F), '
                        'generarlas aleatoriamente (R) o salir (X)?: ')
         if option in ('F', 'R'):
-            cpairs, result1, result2 = '', '', ''
+            cpairs, result1, result2, result0 = '', '', '', ''
             if option == 'F':
                 filename = input('Ingrese el nombre del archivo .txt: ')
                 Xcoords, Ycoords = readCoordsFromFile(filename)
@@ -84,25 +90,37 @@ if __name__ == '__main__':
                 cpairs = cpairs + str(cpair) + ' '
             print(cpairs)
             degree = computeDegree(Xcoords, Ycoords)
-            quoeflist = buildNsolveSystem(degree, Xcoords, Ycoords)
+            X_M, Y_v = buildSystem(Xcoords, Ycoords, degree)
+            quoeflist = np.linalg.lstsq(X_M, Y_v, rcond=None)[0]
+            #print('Quoefficients:', quoeflist)
             pnf_list = np.polyfit(Xcoords, Ycoords, degree)
-            assert len(quoeflist) == len(pnf_list)
+            X_M_T = np.transpose(X_M)
+            if np.linalg.det(X_M_T @ X_M) != 0:
+                calculist = np.linalg.inv(X_M_T @ X_M) @ (X_M_T @ Y_v)
+            else: calculist = np.array([0] * len(Y_v))
+            printCheck(X_M, X_M_T, (X_M_T @ X_M), np.linalg.inv(X_M_T @ X_M), (X_M_T @ Y_v), calculist)
+            assert len(quoeflist) == len(pnf_list) == len(calculist)
             for q in range(len(quoeflist)):
                 if q == degree:
                     result1 += '(' + str(quoeflist[q]) + ')'
                     result2 += '(' + str(pnf_list[q]) + ')'
+                    result0 += '(' + str(calculist[q]) + ')'
                 else:
                     result1 += '(' + str(quoeflist[q]) + ')X^' + str(degree-q) + ' + '
                     result2 += '(' + str(pnf_list[q]) + ')X^' + str(degree-q) + ' + '
+                    result0 += '(' + str(calculist[q]) + ')X^' + str(degree - q) + ' + '
             print('La función polinómica que mejor se ajusta con lstsq es', result1)
             print('La función polinómica que mejor se ajusta con polyfit es', result2)
-            Rmeasure = np.corrcoef(quoeflist, pnf_list)[0, 1] if len(Ycoords) > 1 else 1.0
-            print('La medida de correlación (R) entre ambos métodos es', Rmeasure)
+            print('La función polinómica que mejor se ajusta con analítica es', result0)
+            #Rmeasure = np.corrcoef(quoeflist, pnf_list)[0, 1] if len(Y_v) > 1 else 1.0
+            #print('La medida de correlación (R) entre ambos métodos es', Rmeasure)
             pnf_space = np.arange(-100, 100, 0.01)
             MDQ_fit = np.polyval(quoeflist, pnf_space)
             Autofit = np.polyval(pnf_list, pnf_space)
+            Riskyfit = np.polyval(calculist, pnf_space)
             plt.plot(pnf_space, MDQ_fit, 'r')
             plt.plot(pnf_space, Autofit, 'y')
+            plt.plot(pnf_space, Riskyfit, 'g')
             plt.plot(Xcoords, Ycoords, 'bs')
             plt.show()
         else: break
